e5e58837e56e82b381d0f9d2e650b2c4e434d86e
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-08-15 11:44:51 +0200

    add JsonSerializer.Serialize(weatherForecast)

--- WebApplication_IT_Academy/Program.cs
+++ WebApplication_IT_Academy/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using WebApplication_IT_Academy;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +18,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+var weatherForecast = new WeatherForecast
+{
+    Date = new DateOnly(2024, 8, 15),
+    TemperatureC = 27,
+    Summary = "Hot"
+};
+
+Console.WriteLine(JsonSerializer.Serialize(weatherForecast));
+
 
 app.UseHttpsRedirection();
 
